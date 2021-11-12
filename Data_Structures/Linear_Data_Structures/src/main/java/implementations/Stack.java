bb25441566a2b5dc93992670362777fe18bae1a3
Teodor <te0gop.u@gmail.com>
2021-11-12 15:15:49 +0200

    stack implementation

--- Data_Structures/Linear_Data_Structures/src/main/java/implementations/Stack.java
+++ Data_Structures/Linear_Data_Structures/src/main/java/implementations/Stack.java
@@ -6,33 +6,82 @@ import java.util.Iterator;
 
 public class Stack<E> implements AbstractStack<E> {
 
+    private class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        public Node(E element) {
+            this.value = element;
+        }
+    }
+
+    private Node<E> top;
+    private int size;
+
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
+
     @Override
     public void push(E element) {
+        Node<E> toInsert = new Node<>(element);
+        toInsert.next = top;
+        top = toInsert;
 
+        this.size++;
     }
 
     @Override
     public E pop() {
-        return null;
+        ensureNotEmpty();
+
+        Node<E> tmp = top;
+        top = tmp.next;
+        size--;
+        return tmp.value;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNotEmpty();
+
+        return top.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+
+            private Node<E> current = top;
+
+            @Override
+            public boolean hasNext() {
+                return current.next != null;
+            }
+
+            @Override
+            public E next() {
+                E value = current.value;
+                current = current.next;
+                return value;
+            }
+        };
+    }
+
+    private void ensureNotEmpty() {
+        if(this.isEmpty()) {
+            throw new IllegalStateException();
+        }
     }
 }
