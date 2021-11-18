dde2d0d7bc30ea8a86362e0d363f90e42f29a9dc
Teodor <te0gop.u@gmail.com>
2021-11-18 17:41:36 +0200

    bfs and dfs exercise

--- Data_Structures/Trees_Exercise/src/main/java/implementations/TheMatrix.java
+++ Data_Structures/Trees_Exercise/src/main/java/implementations/TheMatrix.java
@@ -3,7 +3,7 @@ package implementations;
 public class TheMatrix {
     private char[][] matrix;
     private char fillChar;
-    private char toBeReplaced;
+    private char startChar;
     private int startRow;
     private int startCol;
 
@@ -12,15 +12,41 @@ public class TheMatrix {
         this.fillChar = fillChar;
         this.startRow = startRow;
         this.startCol = startCol;
-        this.toBeReplaced = this.matrix[this.startRow][this.startCol];
+        this.startChar = this.matrix[this.startRow][this.startCol];
     }
 
     public void solve() {
-        // TODO: Implement
+        fillMatrix(startRow, startCol);
+    }
+
+    private void fillMatrix(int row, int col) {
+
+        if(isOutOfBounds(row, col) || matrix[row][col] != startChar) {
+            return;
+        }
+
+        matrix[row][col] = fillChar;
+        System.out.println(toOutputString());
+        System.out.println();
+
+        fillMatrix(row + 1, col);
+        fillMatrix(row, col + 1);
+        fillMatrix(row - 1, col);
+        fillMatrix(row, col - 1);
+    }
+
+    private boolean isOutOfBounds(int row, int col) {
+        return row < 0 || row >= matrix.length || col < 0 || col >= matrix[row].length;
     }
 
     public String toOutputString() {
-        // TODO: Implement
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                builder.append(matrix[r][c]);
+            }
+            builder.append(System.lineSeparator());
+        }
+        return builder.toString().trim();
     }
 }
